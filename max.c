660e4a4743274c816c6ad5bfb5af8ae867c5db76
oriharel01-svg <oriharel01@gmail.com>
2025-11-17 12:26:24 +0200

    Add files via upload

--- max.c
+++ max.c
@@ -1,11 +1,15 @@
-
+/*------------------------------------------------------
+* Filename: max.c
+* Description: Program to define two integers and print to console the maximum between them.
+* Author: Ori Harel
+-------------------------------------------------------*/
 
 #include <stdio.h>
 
 int main() {
     int x = 4;
     int y = 6;
-    int z = (x < y) * y + (x > y) * x;
+    int z = (x < y) * y + (x > y) * x + (x == y) * x;
     printf("%d", z);
 
     return 0;
